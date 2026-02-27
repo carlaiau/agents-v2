902aafb1df003c32939ab263f3d9fdcf4ef7cfa8
Carl Aiau <hello@carlaiau.com>
2026-02-27 16:46:52 +1300

    I think there is a bug here mr Moss

--- src/agent/run.ts
+++ src/agent/run.ts
@@ -19,4 +19,97 @@ export async function runAgent(
   userMessage: string,
   conversationHistory: ModelMessage[],
   callbacks: AgentCallbacks,
-): Promise<ModelMessage[]> {}
+): Promise<ModelMessage[]> {
+
+  const workingHistory = filterCompatibleMessages(conversationHistory);
+
+  const messages : ModelMessage[] = [
+    {role: "system", content: SYSTEM_PROMPT},
+    ...workingHistory,
+    {role: "user", content: userMessage}
+  ]
+
+  let fullResponse = "";
+
+  while(true){
+    const result = streamText({
+      model: openai(MODEL_NAME),
+      messages,
+      tools,
+      experimental_telemetry:{
+        isEnabled: true,
+        tracer: getTracer()
+      }
+    })
+
+    const toolCalls: ToolCallInfo[] = []
+
+    let currentText = ""
+    let streamError: Error | null = null
+
+    try{
+      for await (const chunk of result.fullStream){
+        if(chunk.type === "text-delta"){
+          currentText += chunk.text;
+          callbacks.onToken(chunk.text);
+        }
+        if(chunk.type === "tool-call"){
+          const input = 'input' in chunk ? chunk.input: {}
+          toolCalls.push({
+            toolCallId: chunk.toolCallId,
+            toolName: chunk.toolName,
+            args: input as Record<string, unknown>
+          })
+          callbacks.onToolCallStart(chunk.toolName, input )
+        }
+      }
+    } catch(e){
+      streamError = e as Error;
+
+      if(!currentText && !streamError.message.includes("No output generated")){
+        throw streamError;
+      }
+    }
+    fullResponse += currentText;
+    if (streamError && !currentText){
+      fullResponse = "Sorry about that."
+      callbacks.onToken(fullResponse)
+      break;
+    }
+
+    const finishReason = await result.finishReason
+
+    if (finishReason !== "tool-calls" || toolCalls.length === 0){
+      const responseMessages = await result.response;
+      messages.push(...responseMessages.messages)
+      break;
+    }
+
+    // Down here we need to place the tool call request in the messages
+    const responseMessages = await result.response;
+    messages.push(...responseMessages.messages)
+
+    for(const tc of toolCalls){
+      const result = await executeTool(tc.toolName, tc.args)
+      callbacks.onToolCallEnd(tc.toolName, result)
+
+      messages.push({
+        role: "tool",
+        content: [
+          {
+            type: "tool-result",
+            toolCallId: tc.toolCallId,
+            toolName: tc.toolName,
+            output: {
+              type: 'text', 
+              value: result
+            }
+          }
+        ]
+      })
+    }
+  }
+
+  callbacks.onComplete(fullResponse)
+  return messages
+}
